2040aac2de28b8f1e0d719d423693f18eabfa074
Pintu Pandey <psp169142021@gmail.com>
2025-04-21 16:53:41 +0530

    Add files via upload

---
+++ distonary.py
@@ -0,0 +1,39 @@
+# d1={0:100, 1:200, 2:300}
+# d2={0:"hii", 1:"bye"}
+# print(d1)
+# print(d2)
+# print(d1[0]) 
+# print(d2[1])
+# print(d1[0])
+# n1=0b101
+# n2=0o32
+# n3=0xee
+# print(n1)
+# print(n2)
+# print(n3)
+# a="2"
+# b="3"
+# print(a+b)
+# y=10
+# x=y+2
+# print(x)
+# x=(1, 2, 3, 4)
+# print(x[2])
+# name="python"
+# print(name[1])
+# a=int (input("Enter 1st value"))
+# b=int (input("Enter 2nd value"))
+# s=a+b
+# print("sum of value=" , s)
+# a=20
+# b=40
+# print("a+b" , a+b)
+# print("a-b" , a-b)
+# print("a*b" , a*b)
+# print("a/b" , a/b)
+# print("a%b" , a%b)
+# print("a**b" , a**b)
+# for i in range(1,11):
+#   if i==6:
+#     continue  
+# print(i)
